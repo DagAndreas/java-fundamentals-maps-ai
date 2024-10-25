56d298e55b8944bbc2fdef3fe6b5d0ac9e2a0c5c
Dag Andreas <daanfofo@gmail.com>
2024-10-25 10:28:41 +0200

    completed with ai

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -47,8 +47,10 @@ public class Exercise extends ExerciseBase {
          The method must return the value associated to the provided key from the HashMap created
          in the createPerson method
      */
-
-
+    public String getValue(String key) {
+        HashMap<String, String> person = createPerson();
+        return person.get(key);
+    }
 
     /*
         TODO: 2. Create a method named hasKey that accepts two parameters:
@@ -57,8 +59,9 @@ public class Exercise extends ExerciseBase {
          The method must return a boolean that represents whether the string provided exists as a key
          in the provided HashMap
      */
-
-
+    public boolean hasKey(HashMap<String, String> map, String key) {
+        return map.containsKey(key);
+    }
 
     /*
         TODO: 3. Create a method named getValueOrDefault that accepts two parameters:
@@ -67,8 +70,9 @@ public class Exercise extends ExerciseBase {
          The method must use the string provided to return the integer contained in the provided HashMap,
          or -1 if the string provided is not a key in the HashMap
      */
-
-
+    public int getValueOrDefault(HashMap<String, Integer> map, String key) {
+        return map.getOrDefault(key, -1);
+    }
 
     /*
         TODO: 4. Complete the method below
@@ -89,13 +93,17 @@ public class Exercise extends ExerciseBase {
         map.put(7, "muse");
         map.put(96, "nice");
         // Write your code below this comment...
+        ArrayList<String> secretPhrase = new ArrayList<>();
 
-
-
+        for (Integer number : numbers) {
+            if (map.containsKey(number)) {
+                secretPhrase.add(map.get(number));
+            }
+        }
 
         // ...and above this comment
 
         // Change the return statement below to return your actual ArrayList
-        return new ArrayList<String>();
+        return secretPhrase;
     }
 }
